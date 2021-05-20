7865ce92363fb7fe8923aeebf949b9ca8c08b99f
Yanosch <yan_osch@gmx.de>
2021-05-20 21:34:45 +0200

    Add console colors, Add comments

--- Program.cs
+++ Program.cs
@@ -7,23 +7,38 @@ namespace _2._4
     {
         static void Main(string[] args)
         {
+            // Create a new Smartphone object
             Smartphone s = new Smartphone();
+
             while (true)
-            {
+            {   
+                // When the Smartphone is locked, the program asks for a pin
                 if (s.locked)
                 {
+                    // Change text color to red and say that the Smartphone is locked
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Smartphone is locked!");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    // Ask for a pin
                     Console.Write("Please Enter a pin: ");
                     byte[] pin = ASCIIEncoding.ASCII.GetBytes(Console.ReadLine());
                     s.unlock_phone(pin);
                 }
                 else
                 {
+                    // Change text color to green and say that the Smartphone is unlocked
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Smartphone is unlocked!");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+
+                    // Ask what to do next (lock, turn off or do noting)
                     Console.WriteLine("Do you want to lock? ");
                     switch (Console.ReadLine())
                     {
+                        case "yes":
                         case "y": s.lock_phone(); break;
+                        case "turn off":
+                        case "quit":
                         case "q": return;
                         default: Console.WriteLine("You can only lock, unlock or turn off the Smartphone!"); break;
                     }
--- Smartphone.cs
+++ Smartphone.cs
@@ -10,50 +10,72 @@ namespace _2._4
     {
         public bool locked { get; private set; } = true;
         string path = @"pin";
-        byte[] pin_hash = null;
+        byte[] pin_hash;
 
         int lock_time = 5000;
 
         int counter = 0;
         public Smartphone()
         {
-            if (File.Exists(path))
-            {
-                pin_hash = File.ReadAllBytes(path);
-            }
+            // check if a file with a hash exists and load it
+            if (File.Exists(path)) pin_hash = File.ReadAllBytes(path);
 
-            if (pin_hash == null)
-            {
-                get_new_pin();
-            }
+            // if no file exists generate a new pin
+            else get_new_pin();
         }
         private void get_new_pin()
         {
-            Console.Write("Enter new pin: ");
-            byte[] pin = ASCIIEncoding.ASCII.GetBytes(Console.ReadLine());
-            pin_hash = new SHA256Managed().ComputeHash(pin);
-            File.WriteAllBytes(path, pin_hash);
-            Console.WriteLine("\nnew pin created!");
+            string pin;
+            while (true)
+            {
+                // get a pin and check if it is valid (a number)
+                Console.Write("Enter new pin: ");
+                pin = Console.ReadLine();
+                if (Int32.TryParse(pin, out _))
+                {
+                    // string to byte array
+                    byte[] byte_pin = ASCIIEncoding.ASCII.GetBytes(pin);
+                    // hash the pin
+                    pin_hash = new SHA256Managed().ComputeHash(byte_pin);
+                    File.WriteAllBytes(path, pin_hash);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nnew pin created!");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    return;
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Not a vaild pin!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
         }
 
         public bool unlock_phone(byte[] pin)
         {
+            // hash the pin
             byte[] new_hash = new SHA256Managed().ComputeHash(pin);
+
+            // check if the hashs are the same
+            // iterate over each byte and check if they are the same
             for (int i = 0; i < pin_hash.Length; i++)
             {
                 if (pin_hash[i] != new_hash[i])
                 {
+                    // if three times the wrong pin lock the Smartpone for a increasing time
                     counter++;
                     if (counter >= 3)
                     {
                         counter = 0;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Smartphone is locked. Try again in {0} s.", lock_time / 1000);
+                        Console.ForegroundColor = ConsoleColor.Gray;
                         Thread.Sleep(lock_time);
                         lock_time *= 2;
                     }
                     return false;
                 }
             }
+
+            // if the pin is correct unlock the Smartphone
             locked = false;
             return true;
         }
